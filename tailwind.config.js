b5253545aea486563ce9c3cee442ab52ae5ead46
arafathulhairiyya <arafathulhairiyya@gmail.com>
2025-05-24 16:31:51 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './index.html',
+    './product.html',
+    '. *.js',
+    './contact.html',
+    
+
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
